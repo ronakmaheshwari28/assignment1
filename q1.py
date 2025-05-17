6fb80ce3b769eaa9bce09aeea13d503369fb0661
ronakmaheshwari28 <Ronakmalpani28@gmail.com>
2025-05-17 09:59:33 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,12 @@
+n1=float(input("Enter the first number: "))
+n2=float(input("Enter the second number: "))
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+print('Addition: ',add)
+print('Subtraction: ',sub)
+print('Multiplication: ',mul)
+print('Division', div)
+
+
---
+++ q2.py
@@ -0,0 +1,7 @@
+# Take user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+# Concatenate first name and last name into full name
+full_name = first_name + " " + last_name
+# Print a personalized greeting
+print(f"Hello, {full_name}! Welcome to the program.")
